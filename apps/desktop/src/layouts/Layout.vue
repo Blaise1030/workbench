cd190c72b933c0b790109176cc8c8c54ad61865c
blaise1030 <blaisetiong.dev@gmail.com>
2026-04-26 08:09:56 +0800

    feat: updated layout implementation

--- apps/desktop/src/layouts/Layout.vue
+++ apps/desktop/src/layouts/Layout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useAppContext } from "@/app-context/useAppContext";
 import {
   Sidebar,
@@ -26,33 +26,29 @@ import type { Project, Thread } from "@/shared/domain";
 import { useQuery } from "@tanstack/vue-query";
 import { useRoute } from "vue-router";
 import AgentIcon from "@/components/ui/AgentIcon.vue";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const appContext = useAppContext();
 const route = useRoute();
 const projectId = computed(() => route.params.projectId as string);
 const branchId = computed(() => route.params.branch as string);
 
-const { data: worktrees } = useQuery({
-  queryKey: ['worktrees', appContext, projectId],
-  enabled: !!appContext.value.gitService,
-  queryFn: async () => {    
-    const res = await window.workspaceApi?.getSnapshot() as {projects: Project[]}
-    const currentProject = res.projects?.find((p)=> p.id === projectId.value)
-    const path = currentProject?.repoPath!!
-    const worktrees = await appContext.value.gitService.listWorktrees(path)    
-    return worktrees
-  }
-})
+const showMoreToggleState = ref<{ [k: string]: boolean }>({});
 
-const { data: threads } = useQuery({
-  queryKey: ["threads", appContext, projectId],
-  enabled: !!appContext.value.threadManagementService,
+const { data: threadsGroup } = useQuery({
+  queryKey: ["worktrees", appContext, projectId],
+  enabled: !!appContext.value.gitService,
   queryFn: async () => {
+    const res = (await window.workspaceApi?.getSnapshot()) as {
+      projects: Project[];
+    };
+    const currentProject = res.projects?.find((p) => p.id === projectId.value);
+    const path = currentProject?.repoPath!!;
+    const worktrees = await appContext.value.gitService.listWorktrees(path);
     const threads = await appContext.value.threadManagementService.loadThreads(
       projectId.value,
-    );    
-    const reduced = threads.reduce(
+    );
+    const threadsMap = threads.reduce(
       (p, c) => {
         let newP = { ...p };
         if (newP[c.createdBranch ?? ""]) newP[c.createdBranch ?? ""].push(c);
@@ -61,15 +57,15 @@ const { data: threads } = useQuery({
       },
       {} as Record<string, Thread[]>,
     );
-    return Object.keys(reduced).map((key) => {
-      return {
-        label: key,
-        threads: reduced[key]?.map((thread) => ({
-          ...thread,
-          threadPath: `/${projectId.value}/${thread.createdBranch}/thread/${thread.id}`,
-        })),
-      };
-    });
+
+    return worktrees.map(({ path, branch }) => ({
+      path,
+      branch,
+      threads: (threadsMap[branch] ?? [])?.map((thread) => ({
+        ...thread,
+        threadPath: `/${projectId.value}/${thread.createdBranch}/thread/${thread.id}`,
+      })),
+    }));
   },
 });
 </script>
@@ -79,7 +75,7 @@ const { data: threads } = useQuery({
     <Sidebar collapsible="offcanvas">
       <SidebarHeader> Hello </SidebarHeader>
       <SidebarContent class="gap-0 flex flex-col">
-        <SidebarGroup v-for="value in threads" class="gap-0 flex flex-col">
+        <SidebarGroup v-for="value in threadsGroup" class="gap-0 flex flex-col">
           <Collapsible default-open class="group/collapsible p-0">
             <SidebarGroupLabel as-child>
               <CollapsibleTrigger
@@ -88,32 +84,29 @@ const { data: threads } = useQuery({
                 <ChevronRight
                   class="transition-transform group-data-[state=open]/collapsible:rotate-90 mr-1"
                 />
-                {{ value?.label }}
+                {{ value?.branch }}
               </CollapsibleTrigger>
-            </SidebarGroupLabel>            
+            </SidebarGroupLabel>
             <CollapsibleContent>
-              <div class="pb-1 flex gap-0.5"  v-if="branchId === value.label">                
-                <Button size='sm' variant="outline" as-child class="bg-background">
-                  <RouterLink to="/">
-                    Agent
-                  </RouterLink>                  
+              <div class="pb-1 flex gap-0.5" v-if="branchId === value.branch">
+                <Button
+                  size="sm"
+                  variant="outline"
+                  as-child
+                  class="bg-background"
+                >
+                  <RouterLink to="/"> Agent </RouterLink>
                 </Button>
-                <Button size='sm' variant="ghost">
-                  <RouterLink to="/">
-                    Git
-                  </RouterLink>                  
+                <Button size="sm" variant="ghost">
+                  <RouterLink to="/"> Git </RouterLink>
                 </Button>
-                <Button size='sm' variant="ghost">
-                  Files
-                </Button>
-                <Button size='sm' variant="ghost">
-                  Preview
-                </Button>
+                <Button size="sm" variant="ghost"> Files </Button>
+                <Button size="sm" variant="ghost"> Preview </Button>
               </div>
               <SidebarGroupContent>
                 <SidebarMenu>
                   <SidebarMenuItem
-                    v-for="thread in value?.threads"
+                    v-for="thread in showMoreToggleState[value?.branch] ? value?.threads : value?.threads?.splice(0, 10)"
                     :key="thread?.id"
                   >
                     <SidebarMenuButton
@@ -131,6 +124,15 @@ const { data: threads } = useQuery({
                       </RouterLink>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
+                  <Button
+                    v-if="value?.threads.length > 10"                    
+                    size="sm"
+                    variant="link"
+                    class="w-fit bg-background underline"
+                    @click="showMoreToggleState[value?.branch] = !Boolean(showMoreToggleState[value?.branch])"
+                  >
+                    Show {{ showMoreToggleState[value?.branch]? 'less': 'all' }} ({{ value?.threads?.splice(10)?.length }})
+                  </Button>
                 </SidebarMenu>
               </SidebarGroupContent>
             </CollapsibleContent>
